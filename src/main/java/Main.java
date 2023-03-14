4f0c9062070285d2c3abeb297e86dd929f09b9d4
Pauline <pauline.auda@free.fr>
2023-03-14 20:47:30 +0100

    connexion API People et matchContact

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.api.services.people.v1.PeopleService;
+import com.google.api.services.people.v1.model.ListConnectionsResponse;
+import com.google.api.services.people.v1.model.Person;
+
 public class Main{
 
     public static List<String> matchContact(String input){
@@ -36,6 +40,7 @@ public class Main{
             System.out.println("Désolée mais je n'ai pas compris, merci de reformuler votre demande.");
         }
     }
+    
 
     public static void main(String[] args){
         findPlugin();
