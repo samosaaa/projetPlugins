8c4e19c48db438f92cf7104551bd87d88abdd0a8
Pauline <pauline.auda@free.fr>
2023-04-05 11:38:38 +0200

    trucs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main{
 
-    public static List<String> matchContact(String input){
-        Pattern regexContact = Pattern.compile("\\b(né|habite|naissance|âge|connais-tu)\\b", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE |  Pattern.CANON_EQ);
-        Matcher matchContact = regexContact.matcher("(?iu)" + input);
-        List<String> paramsContact = new ArrayList<String>();
-        if (matchContact.find()) {
-            for (int j = 0; j < matchContact.groupCount(); j++) {
-                paramsContact.add(matchContact.group(j + 1));
-            }
-            return paramsContact;
-        }else {
-            return null;
-        }
-    }
-
-    public static void answerContactRequest(String input){
-        
-    }
-
-    public static void findPlugin(){
+    public static void findPlugin() throws GeneralSecurityException, IOException{
         Scanner sc = new Scanner(System.in);
         System.out.println("Comment puis-je vous aider ? ");
         String input = sc.nextLine();
         //switch à la place ?
-        if(matchContact(input)!= null){
-            System.out.println(matchContact(input));
+        if(PeopleQuickstart.matchContact(input) == true){
+            PeopleQuickstart.answerContactRequest(input);
         }
         else{
             System.out.println("Désolée mais je n'ai pas compris, merci de reformuler votre demande.");
@@ -38,7 +18,7 @@ public class Main{
     }
     
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws GeneralSecurityException, IOException{
         findPlugin();
     }
 
