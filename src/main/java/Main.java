e9cbaf7873a89167d89c10101aeb1e7b4e2dc669
Pauline <pauline.auda@free.fr>
2023-04-15 18:15:31 +0200

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-import Controller.ContactFunction;
 import Controller.MainController;
 
 public class Main{
 
     public static void main(String[] args) throws GeneralSecurityException, IOException {
         MainController.findPlugin();
-        System.out.println(ContactFunction.isContact("salut"));
     }
 }
