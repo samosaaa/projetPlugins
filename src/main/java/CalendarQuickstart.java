5bd436bc38a6896b9656d5f84488e8647ce8b6a0
samosaaa <samra_adl@outlook.fr>
2023-04-16 14:13:12 +0200

    to solve

--- src/main/java/CalendarQuickstart.java
+++ src/main/java/CalendarQuickstart.java
@@ -3,17 +3,23 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+
 import com.google.api.client.util.DateTime;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
 import com.google.api.services.calendar.model.Events;
+import com.google.api.services.people.v1.model.Date;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -22,6 +28,7 @@ import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
+import java.util.TimeZone;
 
 /* class to demonstarte use of Calendar events list API */
 public class CalendarQuickstart {
@@ -44,7 +51,28 @@ public class CalendarQuickstart {
    */
   private static final List<String> SCOPES =
       Collections.singletonList(CalendarScopes.CALENDAR);
-  private static final String CREDENTIALS_FILE_PATH = "resources/credentials-calender.json";
+  private static final String CREDENTIALS_FILE_PATH = "resources/credentials.json";
+
+
+ /**
+     * Creates an authorized Credential object.
+     *
+     * @param httpTransport The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final HttpTransport httpTransport) throws IOException {
+      // Load client secrets.
+      GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
+      CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH));
+
+      // Build flow and trigger user authorization request.
+      GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY,
+              clientSecrets, SCOPES).setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                      .setAccessType("offline").build();
+      Credential credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+      return credential;
+  }
 
   /**
    * Creates an authorized Credential object.
@@ -53,7 +81,7 @@ public class CalendarQuickstart {
    * @return An authorized Credential object.
    * @throws IOException If the credentials.json file cannot be found.
    */
-  private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+  private static Credential getCredential(final NetHttpTransport HTTP_TRANSPORT)
       throws IOException {
     // Load client secrets.
     InputStream in = CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
@@ -75,6 +103,17 @@ public class CalendarQuickstart {
   }
 
 /////////////////////////////////////////////////////
+  
+/**
+     * Retrieves the events from the specified calendar.
+     *
+     * @param calendarId The ID of the calendar to retrieve events from.
+     * @param maxResults The maximum number of events to retrieve.
+     * @return A list of events.
+     * @throws IOException              If there was an error communicating with the Calendar API.
+     * @throws GeneralSecurityException If there was a security-related error.
+     */
+
   public static List<Event> getEvents(String calendarId, int maxResults) throws IOException, GeneralSecurityException {
     // Build a new authorized API client service.
     final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
@@ -83,8 +122,54 @@ public class CalendarQuickstart {
 
     // Retrieve the events from the calendar.
     Events events = service.events().list(calendarId).setMaxResults(maxResults).execute();
-    return events.getItems();
-}/////////////////////////////////////////////////////
+    return events.getItems();}
+
+
+    public static void addEvent(String title, Date startDate, Date endDate) throws GeneralSecurityException, IOException {
+      final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+      Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+              .setApplicationName(APPLICATION_NAME).build();
+
+      Event event = new Event().setSummary(title);
+      Date startDateValue = startDate;
+      Date endDateValue = endDate;
+      DateTime start = new DateTime(startDateValue, TimeZone.getDefault());
+      event.setStart(new EventDateTime().setDateTime(start));
+      DateTime end = new DateTime(endDateValue, TimeZone.getDefault());
+      event.setEnd(new EventDateTime().setDateTime(end));
+      service.events().insert("primary", event).execute();
+      System.out.printf("Event created: %s\n", event.getHtmlLink());
+  }
+
+    public void createEvent(String eventSummary, String eventDescription, String eventLocation, String eventTimeZone,
+    java.util.Date date, java.util.Date date2) throws IOException {
+    // Créer une nouvelle instance de l'événement
+    Event event = new Event();
+    event.setSummary(eventSummary);
+    event.setDescription(eventDescription);
+    event.setLocation(eventLocation);
+    event.setTimeZone(eventTimeZone);
+
+    // Définir les dates de début et de fin de l'événement
+    DateTime startDateTime = new DateTime(date);
+    EventDateTime start = new EventDateTime()
+    .setDateTime(startDateTime)
+    .setTimeZone(eventTimeZone);
+    event.setStart(start);
+
+    DateTime endDateTime = new DateTime(date2);
+    EventDateTime end = new EventDateTime()
+    .setDateTime(endDateTime)
+    .setTimeZone(eventTimeZone);
+    event.setEnd(end);
+
+    // Envoyer la requête d'insertion pour ajouter l'événement au calendrier
+    event = calendar.events().insert("primary", event).execute();
+    System.out.printf("Événement ajouté : %s\n", event.getHtmlLink());
+    }
+
+
+/////////////////////////////////////////////////////
 
   public static void main(String... args) throws IOException, GeneralSecurityException {
     // Build a new authorized API client service.
@@ -116,4 +201,5 @@ public class CalendarQuickstart {
       }
     }
   }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,13 @@ import com.google.api.services.people.v1.model.Person;
 public class Main{
 
     public static void findPlugin() throws GeneralSecurityException, IOException{
-            try (Scanner sc = new Scanner(System.in)) {
+        
+        // Charger les informations d'authentification depuis le fichier credentials.json
+        String credentialFilePath = "chemin/vers/credentials.json";
+        CalendarQuickstart calendarQuickstart = new CalendarQuickstart();
+
+        
+        try (Scanner sc = new Scanner(System.in)) {
                 System.out.println("Bonjour, comment puis-je vous aider ? ");
                 String input = sc.nextLine();
 
@@ -29,9 +35,27 @@ public class Main{
                         else if(ContactFunction.matchAddress(input)){
                             InfoContact.addressToString(CONTACT);
                         }
+
                         
-                    }
-                }    
+                    // Ajouter un événement au calendrier
+                    String eventSummary = "Nouvel événement";
+                    String eventDescription = "Description de l'événement";
+                    String eventLocation = "Emplacement de l'événement";
+                    String eventTimeZone = "Fuseau horaire de l'événement";
+                    // Date de début de l'événement
+                    java.util.Calendar startCalendar = java.util.Calendar.getInstance();
+                    startCalendar.set(2023, 4, 15, 9, 0); // 15 mai 2023 à 09:00
+                    // Date de fin de l'événement
+                    java.util.Calendar endCalendar = java.util.Calendar.getInstance();
+                    endCalendar.set(2023, 4, 15, 10, 0); // 15 mai 2023 à 10:00
+
+                    // Appeler la méthode pour ajouter l'événement au calendrier
+                    calendarQuickstart.createEvent(eventSummary, eventDescription, eventLocation, eventTimeZone,
+                            startCalendar.getTime(), endCalendar.getTime());
+
+                    System.out.println("Événement ajouté au calendrier avec succès.");
+                }
+            }
                 else{
                     System.out.println("Désolée mais je n'ai pas compris, merci de reformuler votre demande.");
                 }
